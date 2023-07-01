d20dab306b7b364a6b8de864eca7403d01fb971d
Jemerson Fernando Maia <jfnandopr@hotmail.com>
2023-07-01 18:19:17 +0000

    nosso servico jose maria

--- server.js
+++ server.js
@@ -22,3 +22,6 @@ app.use('/', index);
 
 const jemersonMaiaRoute = require('./src/routes/jemerson-maia');
 app.use('/jemerson-maia', jemersonMaiaRoute);
+
+const joseMariaRoute = require('./src/routes/jose-maria');
+app.use('/jose-maria', joseMariaRoute);
