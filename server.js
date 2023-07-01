5dfbab4b70656ede45ec9c23e553e35a7ee85184
Jaqueline Malman Nunes <jaque.malman@gmail.com>
2023-07-01 18:32:41 +0000

    commit da jaque

--- server.js
+++ server.js
@@ -22,3 +22,6 @@ app.use('/', index);
 
 const jemersonMaiaRoute = require('./src/routes/jemerson-maia');
 app.use('/jemerson-maia', jemersonMaiaRoute);
+
+const jaqueRoute = require('./src/routes/jaque');
+app.use('/jaque', jaqueRoute);
